27faae9a4a8604ef52f0de4d992271a928116b15
miguelruizc <buumchakalaka@gmail.com>
2024-05-29 19:48:32 +0200

    Add Ship class + tests

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 class Ship{
-    constructor(){
-
+    constructor(length = 1){
+        this.length = length;
+        this.hits = 0;
+        this.sunkStatus = false;
     }
 
     sayHi(){
-        return 'hello I am a ship';
+        return 'Hello, I am a ship!';
+    }
+
+    getLength(){
+        return this.length;
+    }
+
+    isSunk(){
+        return this.sunkStatus;
+    }
+
+    takeHit(){
+        this.hits++;
+        if(this.hits >= this.length) this.sunkStatus = true;
     }
 }
 module.exports = {Ship};
--- index.test.js
+++ index.test.js
@@ -3,6 +3,24 @@ const {Ship} = require('./index.js');
 describe('Ship', ()=>{
     test('Say hi', ()=>{
         const ship = new Ship();
-        expect(ship.sayHi()).toBe('hello I am a ship');
+        expect(ship.sayHi()).toBe('Hello, I am a ship!');
+    });
+
+    test('New Ship(5) Creates a ship of length 5', ()=>{
+        const ship = new Ship(5);
+        expect(ship.getLength()).toBe(5);
+    });
+
+    test('Check if ship is sunk', ()=>{
+        const ship = new Ship(1);
+        expect(ship.isSunk()).toBe(false);    
+    });
+
+    test('Ship sinks after getting hit too many times', ()=>{
+        const ship = new Ship(3);
+        ship.takeHit();
+        ship.takeHit();
+        ship.takeHit();
+        expect(ship.isSunk()).toBe(true);
     });
 });
