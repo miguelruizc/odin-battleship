3a605e36fc01546e7991e90f53052b984ce25096
miguelruizc <buumchakalaka@gmail.com>
2024-05-30 13:24:20 +0200

    Add Vite to be able to easily use lodash library

--- DOM_manager.js
+++ DOM_manager.js
@@ -1,3 +1,33 @@
 export class DOM_manager {
 	constructor() {}
+
+	render() {}
+
+	renderBoard(board) {
+		// Take a 2D board representing a board and renders it in the DOM
+		// each row is a div, each cell is a div
+
+		// Board Div
+		const boardDiv = document.getElementById('board1');
+
+		//
+		for (let i = 0; i < board.length; i++) {
+			// Create row
+			const row = document.createElement('div');
+			row.setAttribute('class', 'boardRow');
+
+			for (let j = 0; j < board[i].length; j++) {
+				// Create cell
+				const cell = document.createElement('div');
+				cell.setAttribute('class', 'boardCell');
+				cell.textContent = board[i][j];
+
+				// Append cell
+				row.appendChild(cell);
+			}
+
+			//Append row
+			boardDiv.appendChild(row);
+		}
+	}
 }
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { DOM_manager } from './DOM_manager.js';
+import { Gameboard } from './gameboard.js';
 
-const main = document.getElementById('main');
-main.textContent = 'Hello, world!';
+const dom_manager = new DOM_manager();
+const gameboard = new Gameboard();
+
+dom_manager.renderBoard(gameboard.getBoard());
+
+// TO-DO: Configure webpack to be able to use lodash in the browser
